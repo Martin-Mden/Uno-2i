3fcd65dac4ba13ab5acc4cbf91c7964a370d4359
Martin <m.dennetiere@gmail.com>
2015-05-08 22:54:47 +0200

    Double-clic sur une carte permet de la jouer. Gestion des messages d'erreurs (pas tour du joueur, mauvaise carte et carte non possédée). Gestion de la pioche (BUG: Possibilité de piocher après avoir joué une carte). BUG: Affichage graphique de la défausse non actualisé sur le client de jeu

--- Uno2i-ServeurJeu/src/Metier/Connexion.java
+++ Uno2i-ServeurJeu/src/Metier/Connexion.java
@@ -1,6 +1,7 @@
 
 package Metier;
 
+import Metier.Jeu.Carte;
 import Metier.Jeu.Joueur;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -73,6 +74,15 @@ public class Connexion extends Thread {
                 
                 if(trameEnTete.charAt(0) == 'J' && trameEnTete.charAt(1) == 'C' && trameEnTete.charAt(2) == 'T' && trameEnTete.charAt(3) == 'R') {
                     this.serveur.getPartie().passerTour();
+                }
+                else if(trameEnTete.charAt(0) == 'J' && trameEnTete.charAt(1) == 'C' && trameEnTete.charAt(2) == 'P' && trameEnTete.charAt(3) == 'I') {
+                    this.serveur.getPartie().piocher(joueur);
+                    this.out.println("JSPI/" + joueur.getNom() + ";" + this.serveur.getPartie().getPioche().getDefausse().getId());
+                }
+                else if(trameEnTete.charAt(0) == 'J' && trameEnTete.charAt(1) == 'C' && trameEnTete.charAt(2) == 'J' && trameEnTete.charAt(3) == 'I') {
+                    if(this.serveur.getPartie().jouer(new Carte(trameContenu), joueur)) {                      
+                        this.out.println("JSJR/" + trameContenu + ";" + this.serveur.getPartie().getPioche().getDefausse().getId());
+                    }
                 }
             }
            
--- Uno2i-ServeurJeu/src/Metier/Jeu/Regles.java
+++ Uno2i-ServeurJeu/src/Metier/Jeu/Regles.java
@@ -1,6 +1,8 @@
 
 package Metier.Jeu;
 
+import Outils.Trame;
+
 public class Regles {
     private Partie partie;
 
@@ -20,18 +22,21 @@ public class Regles {
             //On regarde maintenant si la carte jouée par le joueur est bien dans sa main            
             if(!joueur.getMain().possedeCarte(carteJouee)) {
                 System.out.println("[Regles] Le joueur ne possède pas la carte " + carteJouee.getId() + ".");
+                Trame.envoyerPour("JSJE/Message;Vous tentez de jouer une carte que vous ne possédez pas ou plus.", joueur);
                 return false;
             }          
 
             //Si c'est une carte chiffre, (passe ton tour, changement de sens ou +2)
             if(!carteJouee.getCouleur().equals(this.partie.getPioche().getDefausse().getCouleur())) {
                 if(!carteJouee.getValeur().equals(this.partie.getPioche().getDefausse().getValeur())) {
+                    Trame.envoyerPour("JSJE/Message;Vous ne pouvez pas jouer cette carte.", joueur);
                     return false;
                 }
             }                                    
         }
-        else {
+        else {            
             System.out.println("[Regles] Pas le bon joueur!");
+            Trame.envoyerPour("JSJE/Message;Ce n'est pas votre tour.", joueur);
             return false;
         }
         
